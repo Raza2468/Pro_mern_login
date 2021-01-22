7da2a755cf61ef5ec23bcb8d900c203a2af65ace
raza <raza2468@gmail.com>
2021-01-23 00:49:54 +0500

    fir

--- public/app.js
+++ public/app.js
@@ -44,7 +44,7 @@ socket.on("chat-connect", (data) => {
         // console.log(`" email " ${soketloop[i].email}" message "${soketloop[i].msg}" time "${soketloop[i].createdOn}`);
         var post = document.getElementById('welcomeUser')
         var litext = document.createElement('li')
-        var litex=document.createTextNode(`" name=> "${data.name}" massage=> "${data.msg} " time=> "${data.createdOn}`);
+        var litex=document.createTextNode(`" name=> "${data.name}" massage=> "${data.msg} " time=> "${moment(data.createdOn).fromNow()}`);
         litext.appendChild(litex)
         litext.setAttribute("class", "realclass");
             
@@ -280,7 +280,7 @@ function realtimechat() {
             var post = document.getElementById('welcomeUser')
             var litext = document.createElement('li')
             litext.setAttribute("class", "democlass");
-            var litex=document.createTextNode(`" name=> "${loop[i].name} " message=> " ${loop[i].msg}" time=> "${loop[i].createdOn}`);
+            var litex=document.createTextNode(`" name=> "${loop[i].name} " message=> " ${loop[i].msg}" time=> "${moment(loop[i].createdOn).fromNow()}`);
             litext.appendChild(litex);
             post.appendChild(litext);
         }
@@ -293,3 +293,56 @@ function realtimechat() {
 
 }
 
+// function filesumit(){
+//     var file =  document.getElementById("myfile").style.backgroundImage ="url('http://careyofaustin.com/wp-content/uploads/2016/05/car-img.png')";
+// console.log(file);
+
+// }
+function upload() {
+
+    var fileInput = document.getElementById("fileInput");
+
+    // // To convert a File into Blob (not recommended)
+    // var blob = null;
+    // var file = fileInput.files[0];
+    // let reader = new FileReader();
+    // reader.readAsArrayBuffer(file)
+    // reader.onload = function (e) {
+    //     blob = new Blob([new Uint8Array(e.target.result)], { type: file.type });
+    //     console.log(blob);
+    // }
+
+    console.log("fileInput: ", fileInput);
+    console.log("fileInput: ", fileInput.files[0]);
+
+    let formData = new FormData();
+    // https://developer.mozilla.org/en-US/docs/Web/API/FormData/append#syntax
+
+    formData.append("myFile", fileInput.files[0]); // file input is for browser only, use fs to read file in nodejs client
+    // formData.append("myFile", blob, "myFileNameAbc"); // you can also send file in Blob form (but you really dont need to covert a File into blob since it is Actually same, Blob is just a new implementation and nothing else, and most of the time (as of january 2021) when someone function says I accept Blob it means File or Blob) see: https://stackoverflow.com/questions/33855167/convert-data-file-to-blob
+    formData.append("myName", "malik"); // this is how you add some text data along with file
+    formData.append("myDetails",
+        JSON.stringify({
+            // "userEmail": sessionStorage.getItem("userEmail"),
+            "subject": "Science",   // this is how you send a json object along with file, you need to stringify (ofcourse you need to parse it back to JSON on server) your json Object since append method only allows either USVString or Blob(File is subclass of blob so File is also allowed)
+            "year": "2021"
+        })
+    );
+
+    // you may use any other library to send from-data request to server, I used axios for no specific reason, I used it just because I'm using it these days, earlier I was using npm request module but last week it get fully depricated, such a bad news.
+    axios({
+        method: 'post',
+        url: "http://localhost:3001/upload",
+        data: formData,
+        headers: { 'Content-Type': 'multipart/form-data' }
+    })
+        .then(res => {
+            console.log(`upload Success` + res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+    return false; // dont get confused with return false, it is there to prevent html page to reload/default behaviour, and this have nothing to do with actual file upload process but if you remove it page will reload on submit -->
+
+}
--- serverxml.js
+++ serverxml.js
@@ -12,7 +12,7 @@ var { ServerSecretKey } = require("./core/index")
 var socketIo = require("socket.io");
 var http = require("http");
 var { getUser, tweet } = require("./dberor/models")
-
+// var serviceaccount = require("./firebase/firebase.json")
 
 var ServerSecretKey = process.env.SECRET || "123";
 
@@ -27,6 +27,32 @@ appxml.use(cors({
 appxml.use(morgan('dev'));
 
 
+// Firebase bucket
+////// For sending file to mongoose
+const fs = require('fs')
+const multer = require("multer");
+const admin = require("firebase-admin");
+//==============================================
+const storage = multer.diskStorage({ // https://www.npmjs.com/package/multer#diskstorage
+    destination: './uploads/',
+    filename: function (req, file, cb) {
+        cb(null, `${new Date().getTime()}-${file.filename}.${file.mimetype.split("/")[1]}`)
+    }
+})
+//==============================================
+var upload = multer({ storage: storage })
+var serviceAccount = require("./firebase/firebase.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://delete-this-1329.firebaseio.com"
+});
+// const bucket = admin.storage().bucket("gs://delete-this-1329.appspot.com");
+const bucket = admin.storage().bucket("gs://firestore-28544.appspot.com");
+// mongodb+srv://faiz:2468@mundodb.lkd4g.mongodb.net/ttest?retryWrites=true&w=majority
+
+
+// ===============
 // appxml.use(cors());
 // appxml.use(bodyParser.urlencoded({ extended: true }));
 
@@ -159,26 +185,6 @@ appxml.get('/realtimechat', (req, res, next) => {
 });
 
 
-// appxml.get("/myTweets", (req, res, next) => {
-//     console.log("my tweets user=>",req.body);
-//     tweet.find({useremail : req.body.jToken.email},(err,data)=>{
-//       if(!err)
-//       {
-//           console.log("tweet data=>",data);
-//           res.status(200).send({
-//               tweets : data,
-//           });
-//       }
-//       else{
-//           console.log("error : ",err);
-//           res.status(500).send("error");
-//       }
-//     })
-//   });
-
-
-
-
 
 var PORT = process.env.PORT || 3001
 
@@ -187,68 +193,111 @@ var io = socketIo(server, { cors: { origin: "*", methods: "*", } });
 
 
 
+appxml.post("/upload", upload.any(), (req, res, next) => {  // never use upload.single. see https://github.com/expressjs/multer/issues/799#issuecomment-586526877
 
-// io.on('connection', user => {
-//     console.log("connection id", user.id);
-//     // user.broadcast.emit("user-id",user)
-//     user.on('send-message', (message) => {
-//         console.log("message", message);
-//         // user.broadcast.emit("chat-connect",message)
-//     })
-// })
+    console.log("req.body: ", req.body)
+
+    // console.log("req.body: ", JSON.parse(req.body.myDetails));
+    // console.log("req.files: ", req.files);
+
+    // console.log("uploaded file name: ", req.files[0].originalname);
+    // console.log("file type: ", req.files[0].mimetype);
+    // console.log("file name in server folders: ", req.files[0].filename);
+    // console.log("file path in server folders: ", req.files[0].path);
+
+    // upload file to storage bucket 
+    // you must need to upload file in a storage bucket or somewhere safe
+    // server folder is not safe, since most of the time when you deploy your server
+    // on cloud it makes more t2han one instances, if you use server folder to save files
+    // two things will happen, 
+    // 1) your server will no more stateless
+    // 2) providers like heroku delete all files when dyno restarts (their could be lots of reasons for your dyno to restart, or it could restart for no reason so be careful) 
+
+
+    // https://googleapis.dev/nodejs/storage/latest/Bucket.html#upload-examples
+    bucket.upload(
+        req.files[0].path,
+        // {
+        //     destination: `${new Date().getTime()}-new-image.png`, // give destination name if you want to give a certain name to file in bucket, include date to make name unique otherwise it will replace previous file with the same name
+        // },
+        function (err, file, apiResponse) {
+            if (!err) {
+                console.log("api resp: ", apiResponse);
+
+                // https://googleapis.dev/nodejs/storage/latest/Bucket.html#getSignedUrl
+                file.getSignedUrl({
+                    action: 'read',
+                    expires: '03-09-2491'
+                })
+
+                    // console.log("public downloadable url: ", urlData[0]) // this is public downloadable url 
+                    // console.log("my email is => ", userEmail);
+                    // getUser.findById(req.body.jToken.id,
+                    // getUser.find({req.body.email }, {},
+                    // (err, user) => {
+                    .then((urlData, err) => {
+                        if (!err) {
+                            console.log("profileUrl:", urlData[0])
+                            if (!err) {
+                                tweet.find({}, (err, data) => {
+                                    if (!err) {
+                                        console.log("tweetdata=====>", data);
+                                        // res.send({
+                                        //     tweet: data,
+                                        // });
+                                    }
+                                    else {
+                                        console.log("error : ", err);
+                                        res.status(500).send("error");
+                                    }
+                                })
+
+                                // console.log("tweet user===email===> : " + user);
+                            }
+
+                            // tweet.updateMany({ email: email }, { profileUrl: urlData[0] }, 
+                            //     (err, tweet) => {
+                            //     if (!err) {
+                            console.log("profile picture updated succesfully");
+                            //     }
+                            //     // console.log("tweet user===email===> : " + user);
+                            // }
+                            // )
+
+                        }
+                    })
+                // .then((urlData, err) => {
+                //     if (!err) {
+                //         console.log("public downloadable url: ", urlData[0]) // this is public downloadable url 
+                //         console.log("email========eeeeeeee====>", email);
+                //         // // delete file from folder before sending response back to client (optional but recommended)
+                //         // // optional because it is gonna delete automatically sooner or later
+                //         // // recommended because you may run out of space if you dont do so, and if your files are sensitive it is simply not safe in server folder
+                //         // try {
+                //         //     fs.unlinkSync(req.files[0].path)
+                //         //     //file removed
+                //         // } catch (err) {
+                //         //     console.error(err)
+                //         // }
+                //         res.send("Ok");
+                //     }
+                // })
+            } else {
+                console.log("err: ", err)
+                res.status(500).send();
+            }
+        });
+})
+
+
 // ==========================================>Server /////
+
+
+
 
 server.listen(PORT, () => {
     console.log("chal gya hai server", PORT)
 })
 // ==========================================>Server End/////
 
-
-
-// var express = require("express");
-// var path = require("path");
-// var cors = require('cors');
-// var morgan = require('morgan');
-// var bodyParser = require('body-parser');
-// var http = require("http");
-// var socketIO = require('socket.io');
-
-
-// var app= express()
-// app.use(cors())
-// app.use(morgan('dev'))
-// app.use(bodyParser.json())
-
-// var PORT = process.env.PORT || 3001
-
-
-// app.get('/',(res,req,next)=>{
-//     req.send("<h1>Server</h1>")
-
-// })
-
-// app.use("/", express.static(path.resolve(path.join(__dirname,'public'))))
-// var server = http.createServer(app);
-
-// const io = socketIO(server);
-
-// io.on('connection', user=> {console.log("clint id",user.id);});
-// io.on('disconnect', user=>{console.log("dissconect id",user.id);});
-
-// io.on('connection', user => {
-
-// console.log("connection id",user.id);
-// // user.broadcast.emit("user-id",user)
-
-// user.on('send-message',(message)=>{
-//     console.log("message",message);
-//     user.broadcast.emit("chat-connect",message)
-
-
-
-
-// })
-
-// user.on('disconnect', () => { console.log("disconnect id",user.id);});
-// });
 
